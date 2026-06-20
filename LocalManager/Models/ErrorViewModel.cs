f6a6148f9dae76eccef05aafdf475d6fef368e7f
JesusUc18 <jesus.uc@tecdesoftware.edu.mx>
2026-06-19 22:32:03 -0600

    REMOVE: antigua versión de Local Manager (main)

--- LocalManager/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LocalManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
